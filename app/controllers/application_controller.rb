c0b64deba871e391d81357da3e341212615e64a0
M Banks <montensia1@outlook.com>
2019-07-03 15:13:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
-      redirect '/success'
+      redirect '/account'
     else
       redirect '/failure'
     end
